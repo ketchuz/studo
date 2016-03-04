f2b6d1fc755954afb01116f640a967a6be6ee655
Jesus Esparza <jesus.esparza.c4@gmail.com>
2016-03-03 23:22:54 -0600

    Verbs all section bootstrapped

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -75,7 +75,7 @@
   app = angular.module('studo');
 
   app.controller('HomeCtrl', [
-    '$scope', function($scope) {
+    '$scope', '$location', function($scope, $location) {
       $(function() {
         return $('[data-toggle="tooltip"').hover(function() {
           return $(this).tooltip('show');
@@ -83,25 +83,31 @@
           return $(this).tooltip('hide');
         });
       });
+      $scope.GoToAll = function(section) {
+        return $location.path('/' + section + '/all');
+      };
       return $scope.items = [
         {
           title: 'Verbs',
           description: "In this section you will be able to play the verbs you know so far. Not only can you revise its infinitive form but also it's most common future and past form.",
           icon: 'fa fa-bicycle',
           img: 'http://ep1.pinkbike.org/p5pb7291062/p5pb7291062.jpg',
-          order: 'first'
+          order: 'first',
+          section: 'verbs'
         }, {
           title: 'Nouns',
           description: "You might know the rule but...  do you know the gender? Was it 'das', or 'die'? Play this section to help you remember which gender the noun is.",
           icon: 'fa fa-cubes',
           img: 'http://www.desktopdress.com/desktopwallpapers/3d/orange-cube-boxes.jpg',
-          order: 'second'
+          order: 'second',
+          section: 'nouns'
         }, {
           title: 'Adjectives',
           description: 'Beautify your speech. Adjectives are a fundamental and beautiful part of any language. The more you know, the easier you will be able to express what you think.',
           icon: 'fa fa-heart',
           img: 'http://dpshots.com/images/selective_coloring/umbrella_lake.jpg',
-          order: 'third'
+          order: 'third',
+          section: 'adjectives'
         }
       ];
     }
--- app/components/home/homeCtrl.coffee
+++ app/components/home/homeCtrl.coffee
@@ -1,6 +1,6 @@
 app = angular.module 'studo'
 
-app.controller 'HomeCtrl', [ '$scope', ($scope) ->
+app.controller 'HomeCtrl', [ '$scope', '$location', ($scope, $location) ->
 	
 	$ ->
 		$('[data-toggle="tooltip"').hover ->
@@ -8,24 +8,30 @@ app.controller 'HomeCtrl', [ '$scope', ($scope) ->
 		,() -> 
 			$(this).tooltip('hide')
 
+	$scope.GoToAll = (section) ->
+		$location.path '/' + section + '/all'
+
 	$scope.items = [
 			title: 'Verbs'
 			description: "In this section you will be able to play the verbs you know so far. Not only can you revise its infinitive form but also it's most common future and past form."
 			icon: 'fa fa-bicycle'
 			img: 'http://ep1.pinkbike.org/p5pb7291062/p5pb7291062.jpg'
-			order: 'first'
+			order: 'first',
+			section: 'verbs'
 		,
 			title: 'Nouns'
 			description: "You might know the rule but...  do you know the gender? Was it 'das', or 'die'? Play this section to help you remember which gender the noun is."
 			icon: 'fa fa-cubes'
 			img: 'http://www.desktopdress.com/desktopwallpapers/3d/orange-cube-boxes.jpg'
-			order: 'second'
+			order: 'second',
+			section: 'nouns'
 		,
 			title: 'Adjectives'
 			description: 'Beautify your speech. Adjectives are a fundamental and beautiful part of any language. The more you know, the easier you will be able to express what you think.'
 			icon: 'fa fa-heart'
 			img: 'http://dpshots.com/images/selective_coloring/umbrella_lake.jpg'
-			order: 'third'
+			order: 'third',
+			section: 'adjectives'
 	] 
 
 ]
